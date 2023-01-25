05567416669602a6a2d950be6842cd5ec58c94c5
LeesMond <ljm71555555@gmail.com>
2023-01-25 15:34:14 +0900

    여러가지 trigger 생성 및 실행

--- MasterNode/Program.cs
+++ MasterNode/Program.cs
@@ -6,123 +6,124 @@ using System.Timers;
 
 namespace MasterNode
 {
-    public class Program
+    public class date
     {
-
-        private static async Task Main(string[] args)
+        public static string[][] HostInfo = {new string[] { "host1", "jobname1", "2023-01-25T13:25:00", "2023-01-25T15:40:00", "0/5 * * * * ?" }, 
+                                             new string[] { "host2", "jobname2", "2023-01-25T13:25:20", "2023-01-25T15:40:00", "* * * * * ?" }, 
+                                             new string[] { "host3", "jobname3", "2023-01-25T13:25:40", "2023-01-25T15:40:00", "0/10 * * * * ?" } };
+        public class Program
         {
-            int SchduleType = 1;
-            
-            string startDT = "2023-01-18T15:08:00";
-
-            string[] date = startDT.Split("T");
-
-            Console.WriteLine(date[0]);
-            Console.WriteLine(date[1]);
-
-            string[] yearMonthDay = date[0].Split("-");
-            string[] Time = date[1].Split(":");
-            int sec = Int32.Parse(Time[2]);
-            int min = Int32.Parse(Time[1]);
-            int Hour = Int32.Parse(Time[0]);
-
-            int day = Int32.Parse(yearMonthDay[2]);
-            int month = Int32.Parse(yearMonthDay[1]);
-            int year = Int32.Parse(yearMonthDay[0]);
-
-            Console.WriteLine(sec);
-            Console.WriteLine(min);
-            Console.WriteLine(Hour);
-            Console.WriteLine(day);
-            Console.WriteLine(month);
-            Console.WriteLine(year);
-
-
-            if (SchduleType == 0) 
+            private static async Task Main(string[] args)
             {
                 LogProvider.SetCurrentLogProvider(new ConsoleLogProvider());
                 // Grab the Scheduler instance from the Factory
                 StdSchedulerFactory factory = new StdSchedulerFactory();
                 IScheduler scheduler = await factory.GetScheduler();
-
                 await scheduler.Start();
 
-                IJobDetail job = JobBuilder.Create<HelloJob>()
-                    .WithIdentity("job1", "group1")
-                    .Build();
-
-                ITrigger trigger = TriggerBuilder.Create()
-                    .WithIdentity("onetimeTrigger")
-                    .StartAt(DateBuilder.DateOf(Hour, min, sec, day, month, year))
-                    .Build();
-
-                await scheduler.ScheduleJob(job, trigger);
-
-                Console.WriteLine("Job is Done");
-            }
-            else if(SchduleType == 1)
-            {
-                LogProvider.SetCurrentLogProvider(new ConsoleLogProvider());
-                // Grab the Scheduler instance from the Factory
-                StdSchedulerFactory factory = new StdSchedulerFactory();
-                IScheduler scheduler = await factory.GetScheduler();
-
-                await scheduler.Start();
-
-                IJobDetail job = JobBuilder.Create<HelloJob>()
-                    .WithIdentity("job1", "group1")
-                    .Build();
-
-                ITrigger trigger = TriggerBuilder.Create()
-                    .WithIdentity("trigger1", "group1")
-                    .StartAt(DateBuilder.DateOf(Hour, min, sec, day, month, year))
-                    .WithCronSchedule("* * * * * ?")
-                    .EndAt(DateBuilder.DateOf(15, 9, 0))
-                    .Build();
-
-                await scheduler.ScheduleJob(job, trigger);
-
-                Console.WriteLine("Press any key to close the application");
-                Console.ReadKey();
-            }
-            else
-            {
-                Console.WriteLine("this job has error please Check your scheduler again");
-            }
-            
-        }
-        private class ConsoleLogProvider : ILogProvider
-        {
-            public Logger GetLogger(string name)
-            {
-                return (level, func, exception, parameters) =>
+                foreach (string[] arr in date.HostInfo)
                 {
-                    if (level >= LogLevel.Info && func != null)
-                    {
-                        Console.WriteLine("[" + DateTime.Now.ToLongTimeString() + "] [" + level + "] " + func(), parameters);
-                    }
-                    return true;
-                };
-            }
+                    string startDT = arr[2];
+                    string endDT = arr[3];
 
-            public IDisposable OpenNestedContext(string message)
-            {
-                throw new NotImplementedException();
+                    Console.WriteLine(startDT);
+                    Console.WriteLine(endDT);
+
+                    string[] dates = startDT.Split("T");
+                    string[] datee = endDT.Split("T");
+
+                    Console.WriteLine(dates[0]);
+                    Console.WriteLine(dates[1]);
+
+                    string[] yearMonthDays = dates[0].Split("-");
+                    string[] Times = dates[1].Split(":");
+                    int secs = Int32.Parse(Times[2]);
+                    int mins = Int32.Parse(Times[1]);
+                    int Hours = Int32.Parse(Times[0]);
+
+                    int days = Int32.Parse(yearMonthDays[2]);
+                    int months = Int32.Parse(yearMonthDays[1]);
+                    int years = Int32.Parse(yearMonthDays[0]);
+
+                    Console.WriteLine(secs);
+                    Console.WriteLine(mins);
+                    Console.WriteLine(Hours);
+                    Console.WriteLine(days);
+                    Console.WriteLine(months);
+                    Console.WriteLine(years);
+
+                    string[] yearMonthDaye = datee[0].Split("-");
+                    string[] Timee = datee[1].Split(":");
+                    int sece = Int32.Parse(Timee[2]);
+                    int mine = Int32.Parse(Timee[1]);
+                    int Houre = Int32.Parse(Timee[0]);
+
+                    int daye = Int32.Parse(yearMonthDaye[2]);
+                    int monthe = Int32.Parse(yearMonthDaye[1]);
+                    int yeare = Int32.Parse(yearMonthDaye[0]);
+
+                    Console.WriteLine(sece);
+                    Console.WriteLine(mine);
+                    Console.WriteLine(Houre);
+                    Console.WriteLine(daye);
+                    Console.WriteLine(monthe);
+                    Console.WriteLine(yeare);
+
+                    IJobDetail job = JobBuilder.Create<HelloJob>()
+                        .WithIdentity(arr[0], "group1")
+                        .Build();
+
+                    ITrigger trigger = TriggerBuilder.Create()
+                        .WithIdentity(arr[1], "group1")
+                        .StartAt(DateBuilder.DateOf(Hours, mins, secs, days, months, years))
+                        .WithCronSchedule(arr[4])
+                        //.WithSimpleSchedule(x => x
+                        //    .WithIntervalInSeconds(1)
+                        //    .RepeatForever())
+                        .EndAt(DateBuilder.DateOf(Houre, mine, sece, daye, monthe, yeare))
+                        .Build();
+                    
+                    await scheduler.ScheduleJob(job, trigger);
+                    //await scheduler.ScheduleJob(job, trigger);
+                }
+                Console.WriteLine("Press to stop the Schedule");   
+                Console.ReadKey();
+
             }
-            public IDisposable OpenMappedContext(string key, object value, bool destructure = false)
+            private class ConsoleLogProvider : ILogProvider
             {
-                throw new NotImplementedException();
+                public Logger GetLogger(string name)
+                {
+                    return (level, func, exception, parameters) =>
+                    {
+                        if (level >= LogLevel.Info && func != null)
+                        {
+                            Console.WriteLine("[" + DateTime.Now.ToLongTimeString() + "] [" + level + "] " + func(), parameters);
+                        }
+                        return true;
+                    };
+                }
+
+                public IDisposable OpenNestedContext(string message)
+                {
+                    throw new NotImplementedException();
+                }
+                public IDisposable OpenMappedContext(string key, object value, bool destructure = false)
+                {
+                    throw new NotImplementedException();
+                }
             }
         }
-    }
 
-    public class HelloJob : IJob
-    {
-
-        public async Task Execute(IJobExecutionContext context)
+        public class HelloJob : IJob
         {
-            DateTime times = DateTime.Now;
-            await Console.Out.WriteLineAsync("현재 시각 : " + times);
+
+            public async Task Execute(IJobExecutionContext context)
+            {
+                DateTime times = DateTime.Now;
+                await Console.Out.WriteLineAsync("현재 시각 : " + times);
+            }
         }
     }
 }
+
