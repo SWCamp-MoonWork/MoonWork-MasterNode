3995c7636e00b3ad0f6930af8af3d8d753d4ade2
LeesMond <ljm71555555@gmail.com>
2023-01-28 15:22:11 +0900

    Jarray to record

--- MasterNode/Program.cs
+++ MasterNode/Program.cs
@@ -1,30 +1,55 @@
 ﻿using Quartz;
 using Quartz.Impl;
 using Quartz.Logging;
-using System.Diagnostics;
-using System.Timers;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Numerics;
 
 namespace MasterNode
 {
     public class date
     {
-        public static string[][] HostInfo = {new string[] { "host1", "jobname1", "2023-01-26T16:20:00", "2023-01-26T16:20:50", "0/5 * * * * ?", "1.sh" },
-                                             new string[] { "host2", "jobname2", "2023-01-26T16:21:00", "2023-01-26T16:21:20", "* * * * * ?", "2.sh" },
-                                             new string[] { "host3", "jobname3", "2023-01-26T16:22:00", "2023-01-26T16:22:30", "0/10 * * * * ?", "3.sh" } };
+        public static string[][] HostInfo = {new string[] { "host1", "jobname1", "2023-01-27T15:01:00", "2023-01-27T20:00:40", "0/5 * * * * ?", "1.sh", "1" },
+                                             new string[] { "host2", "jobname2", "2023-01-27T15:01:00", "2023-01-27T20:00:40", "* * * * * ?", "2.sh", "0" },
+                                             new string[] { "host3", "jobname3", "2023-01-27T15:01:00", "2023-01-27T20:00:40", "0/10 * * * * ?", "3.sh", "0" } };
 
     }
-    //public record job
-    //{
-    //    public string hostName;
-    //    public string jobName;
-    //    public string startdate;
-    //    public string enddate;
-    //    public string cronexpression;
-    //}
+
+    public record JobModel
+    {
+        long JobId;
+        string? JobName;
+        int IsUse;
+        string? WorkflowName;
+        string? WorkflowBlob;
+        string? Note;
+        DateTime SaveDate;
+        long UserId;
+
+    }
     public class Program
     {
+        static string targetURL = "http://20.39.194.244:5000/v1/job/list";
         private static async Task Main(string[] args)
         {
+            string webclientResult = callWebClient();
+            //var r = JObject.Parse(webclientResult);
+            //var r = JArray.Parse(webclientResult);
+            //var list = r["list"];
+
+            //Console.WriteLine(r);
+
+            string r = JsonConvert.SerializeObject(webclientResult);
+            Console.WriteLine(r);
+            var model = JsonConvert.DeserializeObject<List<JobModel>>(r);
+
+            Console.WriteLine(model);
+
             LogProvider.SetCurrentLogProvider(new ConsoleLogProvider());
             // Grab the Scheduler instance from the Factory
             StdSchedulerFactory factory = new StdSchedulerFactory();
@@ -33,19 +58,12 @@ namespace MasterNode
 
             foreach (string[] arr in date.HostInfo)
             {
-
-                
+                if (arr[6] == "1") continue;
                 string startDT = arr[2];
                 string endDT = arr[3];
 
-                //Console.WriteLine(startDT);
-                //Console.WriteLine(endDT);
-
                 string[] dates = startDT.Split("T");
                 string[] datee = endDT.Split("T");
-
-                //Console.WriteLine(dates[0]);
-                //Console.WriteLine(dates[1]);
 
                 string[] yearMonthDays = dates[0].Split("-");
                 string[] Times = dates[1].Split(":");
@@ -57,13 +75,6 @@ namespace MasterNode
                 int months = Int32.Parse(yearMonthDays[1]);
                 int years = Int32.Parse(yearMonthDays[0]);
 
-                //Console.WriteLine(secs);
-                //Console.WriteLine(mins);
-                //Console.WriteLine(Hours);
-                //Console.WriteLine(days);
-                //Console.WriteLine(months);
-                //Console.WriteLine(years);
-
                 string[] yearMonthDaye = datee[0].Split("-");
                 string[] Timee = datee[1].Split(":");
                 int sece = Int32.Parse(Timee[2]);
@@ -74,30 +85,19 @@ namespace MasterNode
                 int monthe = Int32.Parse(yearMonthDaye[1]);
                 int yeare = Int32.Parse(yearMonthDaye[0]);
 
-                //Console.WriteLine(sece);
-                //Console.WriteLine(mine);
-                //Console.WriteLine(Houre);
-                //Console.WriteLine(daye);
-                //Console.WriteLine(monthe);
-                //Console.WriteLine(yeare);
-
                 IJobDetail job = JobBuilder.Create<HelloJob>()
                     .WithIdentity(arr[0], "group1")
-                    .UsingJobData("name",arr[5])
+                    .UsingJobData("name", arr[5])
                     .Build();
 
                 ITrigger trigger = TriggerBuilder.Create()
                     .WithIdentity(arr[1], "group1")
                     .StartAt(DateBuilder.DateOf(Hours, mins, secs, days, months, years))
                     .WithCronSchedule(arr[4])
-                    //.WithSimpleSchedule(x => x
-                    //    .WithIntervalInSeconds(1)
-                    //    .RepeatForever())
                     .EndAt(DateBuilder.DateOf(Houre, mine, sece, daye, monthe, yeare))
                     .Build();
 
                 await scheduler.ScheduleJob(job, trigger);
-                //await scheduler.ScheduleJob(job, trigger);
             }
             Console.WriteLine("Press to stop the Schedule");
             Console.ReadKey();
@@ -126,6 +126,55 @@ namespace MasterNode
                 throw new NotImplementedException();
             }
         }
+        public static string callWebClient()
+        {
+            string result = string.Empty;
+            try
+            {
+                WebClient client = new WebClient();
+
+                using (Stream data = client.OpenRead(targetURL))
+                {
+                    using (StreamReader reader = new StreamReader(data))
+                    {
+                        string s = reader.ReadToEnd();
+                        result = s;
+
+                        reader.Close();
+                        data.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return result;
+        }
+        public static string callWebRequest()
+        {
+            string responseFromServer = string.Empty;
+
+            try
+            {
+                WebRequest request = WebRequest.Create(targetURL);
+                request.Method = "GET";
+                request.ContentType = "application/json";
+
+                using (WebResponse response = request.GetResponse())
+                using (Stream dataStream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(dataStream))
+                {
+                    responseFromServer = reader.ReadToEnd();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return responseFromServer;
+        }
+
     }
 
     public class HelloJob : IJob
@@ -140,24 +189,25 @@ namespace MasterNode
 
             string jobName = dataMap.GetString("name");
 
-            DateTime times = DateTime.Now; 
-            
+            DateTime times = DateTime.Now;
+
             await Console.Out.WriteLineAsync("현재 시각 : " + times + " / job Name : " + jobName);
 
-            string cmd = "sudo dotnet WorkHost.dll blob/" + jobName;
-            ProcessStartInfo psi = new ProcessStartInfo();
-            psi.UseShellExecute = false;
-            psi.FileName = "bash";
-            psi.Arguments = "-c  \"" + cmd + "\"";
-            try
-            {
-                Process child = Process.Start(psi);
-                child.WaitForExit();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
+            //string cmd = "sudo dotnet WorkHost.dll blob/" + jobName;
+            //ProcessStartInfo psi = new ProcessStartInfo();
+            //psi.UseShellExecute = false;
+            //psi.FileName = "bash";
+            //psi.Arguments = "-c  \"" + cmd + "\"";
+            //try
+            //{
+            //    Process child = Process.Start(psi);
+            //    child.WaitForExit();
+            //}
+
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e.ToString());
+            //}
         }
     }
 }
